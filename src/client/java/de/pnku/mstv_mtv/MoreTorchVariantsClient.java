4362d4377cf0ce654c6930f8873dfd85ec9cce03
pnk2u <159317511+pnk2u@users.noreply.github.com>
2025-06-02 01:06:39 +0200

    Add compatibility with 1.21.6
    BlockRenderLayerMap changes

--- src/client/java/de/pnku/mstv_mtv/MoreTorchVariantsClient.java
+++ src/client/java/de/pnku/mstv_mtv/MoreTorchVariantsClient.java
@@ -3,18 +3,42 @@ package de.pnku.mstv_mtv;
 import de.pnku.mstv_mtv.init.MtvBlockInit;
 import net.fabricmc.api.ClientModInitializer;
 
-import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.world.level.block.Block;
 
 
 public class MoreTorchVariantsClient implements ClientModInitializer {
 
-	
+
 	@Override
 	public void onInitializeClient() {
+		String mcVersion = FabricLoader.getInstance().getModContainer("minecraft").get().getMetadata().getVersion().getFriendlyString();
+		boolean isLegacy = mcVersion.contains("1.21.4") || mcVersion.contains("1.21.5");
 		for (Block torchBlock : MtvBlockInit.more_torch_blocks) {
-			BlockRenderLayerMap.INSTANCE.putBlock(torchBlock, RenderType.cutout());
+			if (isLegacy) {legacyAddToRenderLayerMap(torchBlock);}
+			else {addToRenderLayerMap(torchBlock);}
+		}
+	}
+
+	private void legacyAddToRenderLayerMap(Block block) {
+		try {
+			Class<?> legacyBlockRenderLayerMap = Class.forName("net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap");
+			legacyBlockRenderLayerMap.getMethod("putBlock", Block.class, RenderType.class)
+					.invoke(legacyBlockRenderLayerMap.getField("INSTANCE").get(null), block, RenderType.cutout());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void addToRenderLayerMap(Block block) {
+		try {
+			Class<?> blockRenderLayerMap = Class.forName("net.fabricmc.fabric.api.client.rendering.v1.BlockRenderLayerMap");
+			Class<?> chunkSectionLayerClass = Class.forName("net.minecraft.class_11515");
+			blockRenderLayerMap.getMethod("putBlock", Block.class, chunkSectionLayerClass)
+					.invoke(null, block, chunkSectionLayerClass.getEnumConstants()[2]);
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
 
