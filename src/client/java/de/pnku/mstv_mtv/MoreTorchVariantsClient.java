194cb53cbec7eb9e714de21825c5d237327efb99
pnku <pnku@pnku.de>
2026-04-07 01:28:47 +0200

    Remove usage of BlockRenderLayerMap (1.2.3-d:c1+26.1:c1)
    - No longer necessary in 26.1, handled natively and automatically by the vanilla game

--- src/client/java/de/pnku/mstv_mtv/MoreTorchVariantsClient.java
+++ src/client/java/de/pnku/mstv_mtv/MoreTorchVariantsClient.java
@@ -1,47 +1,9 @@
 package de.pnku.mstv_mtv;
 
-import de.pnku.mstv_mtv.init.MtvBlockInit;
 import net.fabricmc.api.ClientModInitializer;
-
-import net.fabricmc.loader.api.FabricLoader;
-import net.minecraft.client.renderer.rendertype.RenderType;
-import net.minecraft.world.level.block.Block;
 
 
 public class MoreTorchVariantsClient implements ClientModInitializer {
-
-
 	@Override
-	public void onInitializeClient() {
-		String mcVersion = FabricLoader.getInstance().getModContainer("minecraft").get().getMetadata().getVersion().getFriendlyString();
-		boolean isLegacy = mcVersion.contains("1.21.4") || mcVersion.contains("1.21.5");
-		boolean isDev = FabricLoader.getInstance().isDevelopmentEnvironment();
-		for (Block torchBlock : MtvBlockInit.more_torch_blocks) {
-			if (isLegacy) {legacyAddToRenderLayerMap(torchBlock, isDev);}
-			else {addToRenderLayerMap(torchBlock, isDev);}
-		}
-	}
-
-	private void legacyAddToRenderLayerMap(Block block, boolean isDev) {
-		try {
-			Class<?> legacyBlockRenderLayerMap = Class.forName("net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap");
-			String cutoutMethodName = isDev ? "cutout" : "method_23581";
-			legacyBlockRenderLayerMap.getMethod("putBlock", Block.class, RenderType.class)
-					.invoke(legacyBlockRenderLayerMap.getField("INSTANCE").get(null), block, (RenderType) RenderType.class.getMethod(cutoutMethodName).invoke(null));
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	private void addToRenderLayerMap(Block block, boolean isDev) {
-		try {
-			Class<?> blockRenderLayerMap = Class.forName("net.fabricmc.fabric.api.client.rendering.v1.BlockRenderLayerMap");
-			String chunkSectionLayerClassName = isDev ? "net.minecraft.client.renderer.chunk.ChunkSectionLayer" : "net.minecraft.class_11515";
-			Class<?> chunkSectionLayerClass = Class.forName(chunkSectionLayerClassName);
-			blockRenderLayerMap.getMethod("putBlock", Block.class, chunkSectionLayerClass)
-					.invoke(null, block, chunkSectionLayerClass.getEnumConstants()[2]);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+	public void onInitializeClient() {}
 }
